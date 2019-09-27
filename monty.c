9a788f058e28b96c6018c92926928c10f5c32352
DanielBaquero28 <835@holbertonschool.com>
2019-09-27 04:30:15 +0000

    Update [#12]

--- monty.c
+++ monty.c
@@ -12,36 +12,41 @@ global_t gbl = {NULL, NULL};
 
 int main(int argc, char **argv)
 {
-	int line_number;
+	int line_number = 0, label = 0, label2= 0;
 	char *op_token = NULL;
 	stack_t *stack = NULL;
 	size_t len = 0;
 	ssize_t read;
 
 	gbl.line = NULL;
-
 	if (argc != 2)
 		stderr_file_usage();
 	gbl.m_file = fopen(argv[1], "r");
 	if (gbl.m_file == NULL)
-	{
 		stderr_fopen_file(argv[1]);
-	}
-	line_number = 0;
 	read = getline(&(gbl.line), &len, gbl.m_file);
+	    if (gbl.line[0] == '#')
+		    read = getline(&(gbl.line), &len, gbl.m_file);
 	while (read != -1)
-	{
+	{label = 0;
+		label2 = 0;
 		line_number++;
 		op_token = strtok(gbl.line, " \n\a\t\0");
 		op_token2 = strtok(NULL, " \n\a\t\0");
-		get_opcode(&stack, op_token, line_number);
-		read = getline(&(gbl.line), &len, gbl.m_file);
-	}
-	free(op_token2);
+		if (op_token == NULL)
+		{label2 = 1;
+			op_nop(&stack, line_number); }
+		if (label2 == 0)
+		{
+			if (op_token[0] == '#')
+			{read = getline(&(gbl.line), &len, gbl.m_file);
+				label = 1; }}
+		if (label == 0)
+		{get_opcode(&stack, op_token, line_number);
+			read = getline(&(gbl.line), &len, gbl.m_file); }}
 	free(gbl.line);
 	free_stack(stack);
 	gbl.line = NULL;
 	fclose(gbl.m_file);
-
 	exit(EXIT_SUCCESS);
 }
